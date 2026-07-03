664abf503a5756a48a5422be6e934484617ceeb8
Yu Jung-tae <jisdcm29@gmail.com>
2026-07-04 04:24:22 +0800

    Test GitHub Pages root deployment

--- listening-test/auto-submit-on-audio-end.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  "use strict";
-  // STEP53에서 오디오 종료 자동 제출은 사용하지 않습니다.
-  // 시험 종료와 자동 제출은 exam-time-auto-submit.js의 전체 제한 시간 종료 기준으로 처리합니다.
-  window.__topikFinalAudioAutoSubmitHookInstalled = true;
-})();
